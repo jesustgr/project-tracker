9703839b6c9d1f2dc69ae37c6c3cd9f1a833fac9
hyunghung <97567582+hyunghung@users.noreply.github.com>
2023-07-10 11:48:54 -0700

    modal

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ setInterval(dateTime, 1000);
 function dateTime() {
     $("#dateTime").text(dayjs().format('MMM DD, YYYY hh:mm:ss'));
 }
+
+document.getElementById("AddProject").addEventListener("click", function(){
+    form.submit();
+});
+
+var form = document.getElementById("projectsForm");
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
